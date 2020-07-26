c464ce081a6bdc48cac4d1409e7f8967d4879385
Inhye Jeong <58885393+ineainea@users.noreply.github.com>
2020-07-26 23:23:24 +0900

    JS: Navbar menu click

--- js/main.js
+++ js/main.js
@@ -35,3 +35,22 @@ width: 100px; 를 scale(0.5);로(반으로) 줄였을때?
 .getBoundingClientRect()는 width: 50px; 나타냄
 but, offserWidth는 width: 100px; 를 나타냄
 */
+
+//Handle scrolling when tapping on the navbar menu
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event) => {
+
+    //변수 선언
+    const target = event.target;
+    const link = target.dataset.link;
+    //link가 null이거나 undifeined이면 실행x
+    if(link == null){
+        return;
+    }
+
+    //html에 저정한 변수의 데이터가 dataset 안에 할당됨
+    // 속성을 넣어서 각각의 id를 연결시킴
+    console.log(event.target.dataset.link);
+    const scrollTo = document.querySelector(link);
+    scrollTo.scrollIntoView({behavior: "smooth"});
+});
